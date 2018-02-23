07c347ca86be8645611617053ddb741e36272d1b
Peihong Guo <peihongguo@gmail.com>
2018-02-23 02:16:05 -0800

    Corner finder works.

--- corner_finder.cpp
+++ corner_finder.cpp
@@ -6,6 +6,8 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 
+#include "nlohmann/json.hpp"
+
 namespace po = boost::program_options;
 namespace fs = boost::filesystem;
 
@@ -18,7 +20,7 @@ po::variables_map parse_command_line_params(int argc, char **argv) {
     ("board_height", po::value<int>()->default_value(6), "Number of rows on the checker board")
     ("square_size", po::value<float>()->default_value(18), "Checker borad grid cell size in mm")
     ("total_frames", po::value<int>()->default_value(50), "Number of calibration frames")
-    ("output_path", po::value<std::string>()->default_value("./corners"), "Output folder for the detector corners");
+    ("output_path", po::value<std::string>()->default_value("./corners.json"), "Output folder for the detector corners");
 
   po::variables_map vm;
 
@@ -57,19 +59,48 @@ std::pair<bool, cv::Mat> detect_corners(cv::Mat &frame, cv::Size board_size) {
 using frame_object_points_t = std::vector<cv::Point3f>;
 using frame_image_points_t = std::vector<cv::Point2f>;
 
+// TODO: use only 1 set of object points
 struct frame_points_t {
   int frame_index;
   frame_object_points_t object_points;
   frame_image_points_t image_points;
 };
 
+nlohmann::json convert_view_to_json(const frame_points_t& frame_points) {
+  nlohmann::json j;
+  j["index"] = frame_points.frame_index;
+
+  nlohmann::json j_object_points = nlohmann::json::array();
+  for(const auto& p : frame_points.object_points) {
+    j_object_points.push_back({p.x, p.y, p.z});
+  }
+  j["object_points"] = j_object_points;
+
+  nlohmann::json j_image_points = nlohmann::json::array();
+  for(const auto& p : frame_points.image_points) {
+    j_image_points.push_back({p.x, p.y});
+  }
+  j["image_points"] = j_image_points;
+  return j;
+}
+
+nlohmann::json convert_frame_to_json(const std::map<std::string, frame_points_t>& frame) {
+  nlohmann::json j;
+  j["left"] = convert_view_to_json(frame.at("left"));
+  j["right"] = convert_view_to_json(frame.at("right"));
+  return j;
+}
+
+nlohmann::json convert_frames_to_json(const std::vector<std::map<std::string, frame_points_t>>& frames) {
+  nlohmann::json j = nlohmann::json::array();
+  for(const auto& frame : frames) {
+    j.push_back(convert_frame_to_json(frame));
+  }
+  return j;
+}
+
 int main(int argc, char **argv) {
   auto vm = parse_command_line_params(argc, argv);
-
-  fs::path output_path(vm["output_path"].as<std::string>());
-  if(!fs::exists(output_path)) {
-    fs::create_directory(output_path);
-  }
 
   //initialize and allocate memory to load the video stream from camera
   std::unique_ptr<cv::VideoCapture> cams[2] = {
@@ -91,10 +122,11 @@ int main(int argc, char **argv) {
     }
   }
 
-  std::map<std::string, std::vector<frame_points_t>> calibration_data;
+  std::vector<std::map<std::string, frame_points_t>> calibration_data;
   int frame_index = 0;
+  const int total_frames = vm["total_frames"].as<int>();
 
-  while (true) {
+  while (frame_index < total_frames) {
     //grab and retrieve each frames of the video sequentially
     cv::Mat3b frame0, frame1;
     *cams[0] >> frame0;
@@ -105,12 +137,6 @@ int main(int argc, char **argv) {
     std::tie(found0, corners0) = detect_corners(frame0, board_size);
     std::tie(found1, corners1) = detect_corners(frame1, board_size);
 
-    if(found0 && found1) {
-      calibration_data["left"].push_back({frame_index, grid_points, corners0});
-      calibration_data["right"].push_back({frame_index, grid_points, corners0});
-      ++frame_index;
-    }
-
     cv::Mat frame;
     cv::hconcat(std::vector<cv::Mat>{frame0, frame1}, frame);
     cv::imshow("Corner finder", frame);
@@ -120,6 +146,32 @@ int main(int argc, char **argv) {
 
     //exit the loop if user press "Esc" key  (ASCII value of "Esc" is 27)
     if (27 == char(c)) break;
+
+    // save the data when press "Space"
+    if(found0 && found1 && 32 == char(c)) {
+      calibration_data.push_back(
+        std::map<std::string, frame_points_t> {
+          std::make_pair("left", frame_points_t{frame_index, grid_points, corners0}),
+          std::make_pair("right", frame_points_t{frame_index, grid_points, corners1})
+        }
+      );
+
+      ++frame_index;
+      std::cout << frame_index << "/" << total_frames << " recorded." << std::endl;
+    }
+  }
+
+  nlohmann::json j;
+  j["frames"] = convert_frames_to_json(calibration_data);
+  std::cout << j << std::endl;
+
+  const std::string output_filename = vm["output_path"].as<std::string>();
+  std::ofstream fout(output_filename);
+  if(fout) {
+    fout << j << std::endl;
+  } else {
+    std::cerr << "Failed to write " << output_filename << std::endl;
+    exit(1);
   }
 
   return 0;
