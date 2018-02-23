92fed75a0f4238af0a496e58cfc55da7c090b0d2
Peihong Guo <peihongguo@gmail.com>
2018-02-23 01:45:55 -0800

    storing corners in progress.

--- README.md
+++ README.md
@@ -1,6 +1,26 @@
 # Stereo
 
+## Compile
+```bash
+mkdir build && cd build && cmake .. -DCMAKE_BUILD_TYPE=Release && make -j4
+```
+### Dependencies
+* Boost 1.65
+* OpenCV 3.4
+    * Intel MKL (optional)
+    * CUDA (optional)
+
+
 ## Stereo viewer
+```bash
+./stereo_view
+```
 
 ## Corner finder
+```bash
+./corner_finder [cam0] [cam1]
+```
+
 ![alt text](https://github.com/phg1024/Stereo/blob/master/images/corners.png "Corner finder test")
+
+## Single Camera Calibration
--- corner_finder.cpp
+++ corner_finder.cpp
@@ -1,4 +1,4 @@
-#include <boost/filesystem/path.hpp>
+#include <boost/filesystem.hpp>
 #include <boost/program_options.hpp>
 #include <opencv2/opencv.hpp>
 #include <opencv2/core/core.hpp>
@@ -15,7 +15,10 @@ po::variables_map parse_command_line_params(int argc, char **argv) {
     ("left_camera", po::value<int>()->default_value(0), "Left camera id")
     ("right_camera", po::value<int>()->default_value(1), "Right camera id")
     ("board_width", po::value<int>()->default_value(9), "Number of columns on the checker board")
-    ("board_height", po::value<int>()->default_value(6), "Number of rows on the checker board");
+    ("board_height", po::value<int>()->default_value(6), "Number of rows on the checker board")
+    ("square_size", po::value<float>()->default_value(18), "Checker borad grid cell size in mm")
+    ("total_frames", po::value<int>()->default_value(50), "Number of calibration frames")
+    ("output_path", po::value<std::string>()->default_value("./corners"), "Output folder for the detector corners");
 
   po::variables_map vm;
 
@@ -51,8 +54,22 @@ std::pair<bool, cv::Mat> detect_corners(cv::Mat &frame, cv::Size board_size) {
   return std::make_pair(found, corners);
 }
 
+using frame_object_points_t = std::vector<cv::Point3f>;
+using frame_image_points_t = std::vector<cv::Point2f>;
+
+struct frame_points_t {
+  int frame_index;
+  frame_object_points_t object_points;
+  frame_image_points_t image_points;
+};
+
 int main(int argc, char **argv) {
   auto vm = parse_command_line_params(argc, argv);
+
+  fs::path output_path(vm["output_path"].as<std::string>());
+  if(!fs::exists(output_path)) {
+    fs::create_directory(output_path);
+  }
 
   //initialize and allocate memory to load the video stream from camera
   std::unique_ptr<cv::VideoCapture> cams[2] = {
@@ -66,6 +83,17 @@ int main(int argc, char **argv) {
   const int board_height = vm["board_height"].as<int>();
   cv::Size board_size(board_width, board_height);
 
+  const float square_size = vm["square_size"].as<float>();
+  frame_object_points_t grid_points;
+  for(int i=0;i<board_height;++i) {
+    for(int j=0;j<board_width;++j) {
+      grid_points.push_back(cv::Point3f(j * square_size, i * square_size, 0.f));
+    }
+  }
+
+  std::map<std::string, std::vector<frame_points_t>> calibration_data;
+  int frame_index = 0;
+
   while (true) {
     //grab and retrieve each frames of the video sequentially
     cv::Mat3b frame0, frame1;
@@ -76,6 +104,12 @@ int main(int argc, char **argv) {
     cv::Mat corners0, corners1;
     std::tie(found0, corners0) = detect_corners(frame0, board_size);
     std::tie(found1, corners1) = detect_corners(frame1, board_size);
+
+    if(found0 && found1) {
+      calibration_data["left"].push_back({frame_index, grid_points, corners0});
+      calibration_data["right"].push_back({frame_index, grid_points, corners0});
+      ++frame_index;
+    }
 
     cv::Mat frame;
     cv::hconcat(std::vector<cv::Mat>{frame0, frame1}, frame);
